9c22f491acd41c48ce9f9adb7f8a0209129dc079
Cesar <czarvoigt@gmail.com>
2023-07-02 00:37:51 -0300

    Teste

---
+++ NotacaoPonto.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+public class NotacaoPonto {
+
+	public static void main(String[] args) {
+		
+		String s = "Bom dia X!";
+		s = s.toUpperCase();
+		
+		s = s.replace("X", "Senhora");
+		
+		System.out.println(s);
+		
+		String x = "Leo".toUpperCase();
+		System.out.println(x);
+		
+		//pode quebrar o código em multiplas linhas
+		String y = "Bom dia X"
+				.replace("X","Gui")
+				.toUpperCase()
+				.concat("!!!");
+		System.out.println(y);
+		
+		//tipos primitivos não tem o operador "."
+		
+	}
+
+}
