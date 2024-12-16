03079ca0b3650d80f747fae49d77fa7d7e7d25c5
César Voigt <czarvoigt@gmail.com>
2024-12-15 21:44:48 -0300

    NewClass

---
+++ Potencia.java
@@ -0,0 +1,13 @@
+public class Potencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-genera	ted method stub
+		int a = 2;
+		int c = (int) Math.pow(a, 3);
+		double b = Math.pow(a, 3);
+		
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
